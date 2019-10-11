904883833e9683195306cc7ae5fc97eae2470dc5
Siddharth Panda <siddharth.pandathedancer@gmail.com>
2019-10-11 23:03:40 +0530

    Converted to python3

--- ImageHandler.py
+++ ImageHandler.py
@@ -11,131 +11,129 @@ from HistogramProcessing import HistogramOperations
 import matplotlib.image as mpimg
 import os
 
+
 class Imagehandler(object):
-    def __init__(self,path,img=None):
-        print path
-        if (os.path.exists(path) and os.path.isfile(path) ):
-            self.ImagePath=path
+    def __init__(self, path, img=None):
+        print(path)
+        if (os.path.exists(path) and os.path.isfile(path)):
+            self.ImagePath = path
             if img is None:
-                self.Image=cv.imread(self.ImagePath,0)
-                print self.Image.shape[:2]
+                self.Image = cv.imread(self.ImagePath, 0)
+                print(self.Image.shape[:2])
 #                cv.imshow("hello",self.Image)
 #                cv.waitKey(0)
             else:
-                self.Image=img
-                print self.Image.shape[:2]
-            
+                self.Image = img
+                print(self.Image.shape[:2])
+
         else:
             print("file not found")
-    
+
     def __convertImagetoBlackWhite(self):
-        self.Image=cv.imread(self.ImagePath,cv.IMREAD_COLOR)
-        self.imageOriginal=self.Image
+        self.Image = cv.imread(self.ImagePath, cv.IMREAD_COLOR)
+        self.imageOriginal = self.Image
         if(self.Image is None):
             print("some problem with the image")
         else:
             print("Image Loaded")
-            
-        self.Image=cv.cvtColor(self.Image,cv.COLOR_BGR2GRAY)
-        self.Image=cv.adaptiveThreshold(self.Image,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,11,2)
+
+        self.Image = cv.cvtColor(self.Image, cv.COLOR_BGR2GRAY)
+        self.Image = cv.adaptiveThreshold(
+            self.Image, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
         return self.Image
-    
-    def WritingImage(self,image,path,imageName):
+
+    def WritingImage(self, image, path, imageName):
         if(image is None):
             print("Image is not valid.Please select some other image")
         else:
-            image=cv.cvtColor(image,cv.COLOR_BGR2RGB)
+            image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
             print(path+imageName)
-            cv.imwrite(path+imageName,image)
-            cv.imshow(imageName,image)
-            cv.waitKey(0);
+            cv.imwrite(path+imageName, image)
+            cv.imshow(imageName, image)
+            cv.waitKey(0)
 
-
-    
-    def ImagesToTiles(self,tileX,tileY):
-        imageHeight,imageWidth=self.Image.shape[:2]
-        imageHeight=int(imageHeight/tileY)*tileY
-        imageWidth=int(imageWidth/tileX)*tileX
-        self.Image = cv.resize(self.Image,(imageWidth,imageHeight), interpolation = cv.INTER_CUBIC)
-        #cv.resize takes shape as (width,height) instead of height,width
-        i=0
-        j=0
-        featureVector=[]
-        #print imageHeight
+    def ImagesToTiles(self, tileX, tileY):
+        imageHeight, imageWidth = self.Image.shape[:2]
+        imageHeight = int(imageHeight/tileY)*tileY
+        imageWidth = int(imageWidth/tileX)*tileX
+        self.Image = cv.resize(
+            self.Image, (imageWidth, imageHeight), interpolation=cv.INTER_CUBIC)
+        # cv.resize takes shape as (width,height) instead of height,width
+        i = 0
+        j = 0
+        featureVector = []
+        # print imageHeight
         # print imageWidth
-        count=0
+        count = 0
 #        im = mpimg.imread(self.ImagePath)
 #        plt.imshow(im)
 #        f, axarr = plt.subplots(tileX, tileY)
-        while i< imageHeight:
-            j=0
+        while i < imageHeight:
+            j = 0
             while j < imageWidth:
-                img=self.Image[i:i+tileX,j:j+tileY]
-                if len(img)==0:
+                img = self.Image[i:i+tileX, j:j+tileY]
+                if len(img) == 0:
                     continue
 #                cv.rectangle(self.Image,(j,i), (j+tileY,i+tileX), (0,255,0),3)
-                subTilesList=self.SubDivideTile(img,4,4)
+                subTilesList = self.SubDivideTile(img, 4, 4)
 #                print subTilesList
-                histogramSubTiles=[]
+                histogramSubTiles = []
 #                    print subTilesList
                 for tile in subTilesList:
-#                    print tile
-                    
-                    tileObj=HistogramOperations(tile)
-                    akrusd=tileObj.HistogramOfGradient(tile,9)
+                    #                    print tile
+
+                    tileObj = HistogramOperations(tile)
+                    akrusd = tileObj.HistogramOfGradient(tile, 9)
                     histogramSubTiles.append(akrusd)
-                nonsense=tileObj.ConcatAndNormalisationofHistogram(histogramSubTiles)
-                featureVector.append(nonsense)  
+                nonsense = tileObj.ConcatAndNormalisationofHistogram(
+                    histogramSubTiles)
+                featureVector.append(nonsense)
 #                print featureVector
 #                y_pos = np.arange(9)
 #                axarr[i,j].bar(y_pos, nonsense, align='center', alpha=0.5)
-                
-                j=j+tileY
-            count +=1
+
+                j = j+tileY
+            count += 1
 #            print ("next loop")
 #            print ( count,i)
-            i=i+tileX
-        #image should be shown at last as image is changed after every rectangle,square,cirlce operation
-        featureVector=HistogramOperations.ConcatFeatureVectors(featureVector)
+            i = i+tileX
+        # image should be shown at last as image is changed after every rectangle,square,cirlce operation
+        featureVector = HistogramOperations.ConcatFeatureVectors(featureVector)
 #        cv.imshow("hi",self.Image)
 #        plt.show()
 #        cv.waitKey(0)
 #        except:
 #            print featureVector
 
-        
+
 #        print ("DONE")
         return featureVector
-        
-    def SubDivideTile(self,inputTile,tileX,tileY):
-        #print inputTile
-        imageHeight,imageWidth=inputTile.shape[:2]
-        imageHeight=int(imageHeight/tileY)*tileY
-        imageWidth=int(imageWidth/tileX)*tileX
+
+    def SubDivideTile(self, inputTile, tileX, tileY):
+        # print inputTile
+        imageHeight, imageWidth = inputTile.shape[:2]
+        imageHeight = int(imageHeight/tileY)*tileY
+        imageWidth = int(imageWidth/tileX)*tileX
         try:
-            tile = cv.resize(inputTile,(imageWidth,imageHeight), interpolation = cv.INTER_CUBIC)
+            tile = cv.resize(inputTile, (imageWidth, imageHeight),
+                             interpolation=cv.INTER_CUBIC)
         except:
-            print (inputTile)
+            print(inputTile)
             return
-        subTilesList=[]
-        i=0
-        j=0
-        #count=1
-        while i< imageHeight:
-            j=0
+        subTilesList = []
+        i = 0
+        j = 0
+        # count=1
+        while i < imageHeight:
+            j = 0
             while j < imageWidth:
-                img=tile[i:i+tileX,j:j+tileY]
+                img = tile[i:i+tileX, j:j+tileY]
                 subTilesList.append(img)
 #                cv.rectangle(self.Image,(j,i), (j+tileY,i+tileX), (255,0,0),3)
                # print img
-                j=j+tileY
+                j = j+tileY
 
 #                count +=1
-            i=i+tileX
+            i = i+tileX
        # print "adasdadadadadadas",count)
-        return subTilesList   
-    
-
-        
-    
-        
+        return subTilesList
--- utility.py
+++ utility.py
@@ -7,18 +7,20 @@ Created on Tue Oct 10 17:20:27 2017
 """
 import cv2 as cv
 
-def Pyramid(img,scale,minsize):
+
+def Pyramid(img, scale, minsize):
     "this function is used to creeate the guassian pyramid of the images"
     yield img
-    imageHeight,imageWidth=img.shape[:2]
-    while imageHeight >minsize[0]  and imageWidth>minsize[1]:
-        imageHeight=int(imageHeight/scale)
-        imageWidth=int(imageWidth/scale)
-        img=cv.resize(img,(imageWidth,imageHeight), interpolation = cv.INTER_CUBIC)
+    imageHeight, imageWidth = img.shape[:2]
+    while imageHeight > minsize[0] and imageWidth > minsize[1]:
+        imageHeight = int(imageHeight/scale)
+        imageWidth = int(imageWidth/scale)
+        img = cv.resize(img, (imageWidth, imageHeight),
+                        interpolation=cv.INTER_CUBIC)
         yield img
-        
+
+
 def SlidingWindow(image, stepSize, windowSize):
-    for x in xrange(0, image.shape[0], stepSize[0]):
-		for y in xrange(0, image.shape[1], stepSize[1]):
-			yield (x, y, image[x:x + windowSize[1],x:x + windowSize[0] ])      
-        
+    for x in range(0, image.shape[0], stepSize[0]):
+        for y in range(0, image.shape[1], stepSize[1]):
+            yield (x, y, image[x:x + windowSize[1], x:x + windowSize[0]])
